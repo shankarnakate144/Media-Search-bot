dc43f625ec42aabaa884f0a1cff8ed7f6c3eeafe
shankarnakate144 <117718346+shankarnakate144@users.noreply.github.com>
2022-11-08 19:42:02 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 13734154
+API_HASH = '016fbb94a12779228db87b599395f078'
+BOT_TOKEN = '5692164380:AAGjyZT4OIVWm1UjfOpqKU9ysKw6k9SNKpc
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,22 +11,24 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [13734154, 'official_shankya2
+CHANNELS = [-@officialshankya
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://shankya:shankya@cluster0.k8dmt8s.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
 START_MSG = """
-**Hi, I'm Media Search bot**
+**Hi, I'm Media Search bot
+
+Just try it once and join my movie update channel @officialshankya**
 
 Here you can search files in inline mode. Just press follwing buttons and start searching.
 """
 
-SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
-INVITE_MSG = 'Please join @.... to use this bot'
+SHARE_BUTTON_TEXT = 'Checkout {@officialshankya} for searching files'
+INVITE_MSG = 'Please join @officialshankya'
